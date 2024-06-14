e252eef54a9be829b6cb8e5b688bb27d20a79ab8
William Mear <williammear@yahoo.com>
2024-06-14 16:22:35 +0100

    configure config server

--- backend/core-service/src/main/java/com/willmear/core_service/controller/CoreController.java
+++ backend/core-service/src/main/java/com/willmear/core_service/controller/CoreController.java
@@ -1,2 +1,18 @@
-package com.willmear.core_service.controller;public class CoreController {
+package com.willmear.core_service.controller;
+
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoreController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+
 }
