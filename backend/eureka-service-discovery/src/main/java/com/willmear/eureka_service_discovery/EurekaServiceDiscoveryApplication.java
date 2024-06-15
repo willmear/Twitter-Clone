2fc7832bf1877eae2325667d672ed6737da750a4
William Mear <williammear@yahoo.com>
2024-06-15 07:10:07 +0100

    create API Gateway

--- backend/eureka-service-discovery/src/main/java/com/willmear/eureka_service_discovery/EurekaServiceDiscoveryApplication.java
+++ backend/eureka-service-discovery/src/main/java/com/willmear/eureka_service_discovery/EurekaServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.willmear.eureka_service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceDiscoveryApplication {
 
