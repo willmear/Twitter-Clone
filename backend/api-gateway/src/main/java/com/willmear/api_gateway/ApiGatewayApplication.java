d5e6c27ab0774999c5a47adcb633741bdd9028a2
William Mear <williammear@yahoo.com>
2024-06-16 16:50:15 +0100

    routing for api gateway

--- backend/api-gateway/src/main/java/com/willmear/api_gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/willmear/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,22 @@
 package com.willmear.api_gateway;
 
+import org.apache.commons.io.filefilter.PrefixFileFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
 
 }
