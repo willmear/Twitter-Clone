0c6937e6552826db05b53a43fbbb5d468ace2954
William Mear <williammear@yahoo.com>
2024-06-16 20:19:44 +0100

    update

---
+++ backend/api-gateway/src/main/java/com/willmear/api_gateway/config/RouteConfiguration.java
@@ -0,0 +1,23 @@
+package com.willmear.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RouteConfiguration {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("core-service", r -> r.path("/core-service/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://core-service"))
+                        .build();
+
+    }
+
+}
